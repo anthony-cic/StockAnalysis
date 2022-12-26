627509471d9a7a5840f9378ff9b79cd61332e0dc
anthony-cic <69987069+anthony-cic@users.noreply.github.com>
2022-12-25 23:58:26 -0500

    moved function to perchange file

--- main.py
+++ main.py
@@ -1,47 +1,22 @@
 import yfinance as yf
 
-
-
-def analyze(df, key):
-    # analyzes a key year over year for a given stock
-    # stock parameter must be a yf.ticker data type 
-    # returns a dictionary with the percent change in cashflow from last year 
-
-
-    i = 0 
-    perChangeDict = {} 
-    for date in df.keys():
-        if (i + 1 == len(df.keys())):
-            break 
-
-        amt = df[date][key]
-        prev = df[df.keys()[i+1]][key]
-
-        perChange = float((amt - prev) / prev)*100
-        perChange = round(perChange, 2) 
-
-        perChangeDict[df.keys()[i].year] = perChange
-        i += 1
-    
-    return perChangeDict
-
-
+from perChange import perChange
 
 def main(): 
-    #ticker = input("Input the stock ticker to analyze: ")
-    ticker = "AAPL" 
+    ticker = input("Input the stock ticker to analyze: ")
+    #ticker = "AAPL" 
 
     stock = yf.Ticker(ticker) 
 
     print(f"=================================== \n{ticker} Stock Analysis...\n")
 
     cashFlow = stock.cashflow 
-    cashFlowDict = analyze(cashFlow, 'Free Cash Flow')
+    cashFlowDict = perChange(cashFlow, 'Free Cash Flow')
 
     print(f"Free Cash Flow change percent (from prev year): {cashFlowDict}")
 
     income_stmt = stock.income_stmt
-    revenueDict = analyze(income_stmt, 'Total Revenue')
+    revenueDict = perChange(income_stmt, 'Total Revenue')
 
     print(f"Revenue change percent (from prev year): {revenueDict}")
     
